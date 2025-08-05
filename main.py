ba3d41aa3bc49e165967c191837af564db4fc5ae
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-05 12:56:22 +0430

    frame for tkinter.

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ window.resizable(True, True)
 window.configure(background="blue")
 
 file_label = ttk.Label(window, text= "Select the file participants.", font=('Arial', 12), background='', foreground='')
+style = ttk.Style()
+style.configure("TFream", background = '')
+
 file_label.pack(pady=15)
+file_frame = ttk.Frame(window)
+file_frame.pack()
+file_entry = ttk.Entry(file_frame, font=('Arial', 12))
+file_entry.grid(row=0, column=0, padx=5, pady=5)
 
 window.mainloop()
