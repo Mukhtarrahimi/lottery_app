ebcc6a50a00e26e2a2b0319f13832418f6c98b18
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-05 13:03:02 +0430

    select_file function.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 
+def select_file():
+    file_path = filedialog.askopenfilename(filetypes=[('Text File', '*.txt')])
+    file_entry.delete(0, tk.END)
+    file_entry.insert(ttk.END, file_path)
+    
+
 window = tk.Tk()
 window.title("lottery_app")
 window.geometry("500 * 400")
@@ -16,5 +22,8 @@ file_frame = ttk.Frame(window)
 file_frame.pack()
 file_entry = ttk.Entry(file_frame, font=('Arial', 12))
 file_entry.grid(row=0, column=0, padx=5, pady=5)
+
+file_btn = ttk.Button(file_frame, text= 'select file', compound=select_file)
+file_btn.grid(row= 0, column= 0, padx=0, pady= 5)
 
 window.mainloop()
