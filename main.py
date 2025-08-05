94aedecacdde209609f2c44225abaafa734b35fb
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-05 12:51:46 +0430

    tkinter code.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 
+window = tk.Tk()
+window.title("lottery_app")
+window.geometry("500 * 400")
+window.resizable(True, True)
+window.configure(background="blue")
+
+file_label = ttk.Label(window, text= "Select the file participants.", font=('Arial', 12), background='', foreground='')
+file_label.pack(pady=15)
+
+window.mainloop()
